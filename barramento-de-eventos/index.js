1ad46580ff9aa3347bf353609bed17305cd25a82
ricardon91 <ricardo_l.h.p@hotmail.com>
2021-10-24 11:47:37 -0300

    inserido dockerfile e .dockerignore

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -6,8 +6,11 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const eventos = [];
+
 app.post('/eventos', (req, res) => {
     const evento = req.body;
+    eventos.push(evento)
     //envia o evento para o microsserviço de lembretes
     axios.post('http://localhost:4000/eventos', evento);
     //envia o evento para o microsserviço de observações
@@ -18,6 +21,10 @@ app.post('/eventos', (req, res) => {
     axios.post("http://localhost:7000/eventos", evento);
     res.status(200).send({ msg: "ok" });
 });
+
+app.get('/eventos', (req, res) => {
+    res.send(eventos)
+})
 
 app.listen(10000, () => {
     console.log('Barramento de eventos. Porta 10000.')
--- consulta/index.js
+++ consulta/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 
@@ -34,4 +35,15 @@ app.post("/eventos", (req, res) => {
     res.status(200).send(baseConsulta);
 });
 
-app.listen(6000, () => console.log("Consultas. Porta 6000"));
+app.listen(6000, async () => {
+    console.log("Consultas. Porta 6000");
+    const resp = await
+
+        axios.get("http://localhost:10000/eventos");
+    //axios entrega os dados na propriedade data
+    resp.data.forEach((valor, indice, colecao) => {
+        try {
+            funcoes[valor.tipo](valor.dados);
+        } catch (err) { }
+    });
+});
